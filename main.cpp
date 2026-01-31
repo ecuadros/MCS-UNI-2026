b73d05495a3cf700c73e80501b4411acff109016
adderdiazt-ad <258343007+adderdiazt-ad@users.noreply.github.com>
2026-01-31 16:02:05 -0500

    Up date .cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <vector>
 
 int main(){
     std::cout << "MCS-2026\n";
+    std::vector<double> calificaciones;
+    calificaciones.push_back(10.0);
+    calificaciones.push_back(9.5);
+    calificaciones.push_back(8.7);
+    std::cout << "La primera calificacion es: " << calificaciones[0] << std::endl;
     return 0;
 }
