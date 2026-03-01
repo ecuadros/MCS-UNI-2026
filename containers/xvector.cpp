d30343b6b40092cf0be54de17c8ec771c9a1d01c
ecuadros <ecuadros@spc.org.pe>
2026-02-28 21:57:47 -0500

    feat: Refactor XVector to use traits for type handling, add a `toString` method, and output demo data to a file.

--- containers/xvector.cpp
+++ containers/xvector.cpp
@@ -1,27 +1,34 @@
+#include <fstream>
 #include "xvector.h"
 #include "../types.h"
 
 void DemoVector() {
-    XVector<T1> v;
+    XVector< VectorTraits<T1> > v1;
     auto x = 20;
-    v.PushBack(5);
-    v.PushBack(2);
-    v.PushBack(8);
-    v[1] = 15;
-    cout << v << endl;
-    v.PushBack(10); 
-    cout << v << "Otro texto: " << x << endl;
+    v1.PushBack(5);
+    v1.PushBack(2);
+    v1.PushBack(8);
+    v1[1] = 15;
+    cout << v1 << endl;
+    v1.PushBack(10); 
+    cout << v1 << "Otro texto: " << x << endl;
 
-    XVector<T2> v2;
+    XVector< VectorTraits<T2> > v2;
     v2.PushBack(3.5);
     v2.PushBack(2.5);
     v2.PushBack(1.5);
     cout << v2 << endl;
 
-    XVector<T3> v3;
+    XVector< VectorTraits<T3> > v3;
     v3.PushBack("Hola");
-    v3.PushBack("MCS");
+    v3.PushBack("MCS"); 
     v3.PushBack("UNI");
     v3.PushBack("2026");
     cout << v3 << endl;
+
+    ofstream of("Output.txt");
+    of << v1 << endl;
+    of << v2 << endl;
+    of << v3 << endl;
+    of.close();
 }
--- containers/xvector.h
+++ containers/xvector.h
@@ -6,26 +6,34 @@
 using namespace std;
 
 template <typename T>
+struct VectorTraits {
+    using value_type = T;
+};
+
+template <typename Traits>
 class XVector {
+    using value_type = typename Traits::value_type;
+
 private:
-    T     *m_data;
-    size_t m_size;
-    size_t m_capacity;
+    value_type     *m_data;
+    size_t          m_size;
+    size_t          m_capacity;
 
 public:
     XVector() : m_data(nullptr), m_size(0), m_capacity(0) {}
     virtual ~XVector() { delete[] m_data; }
 
-    void PushBack(const T &value);
-    T &operator[](size_t index);
+    void PushBack(const value_type &value);
+    value_type &operator[](size_t index);
     size_t Size() const { return m_size; }
+    string toString() const;
 };
 
-template <typename T>
-void XVector<T>::PushBack(const T &value) {
+template <typename Traits>
+void XVector<Traits>::PushBack(const value_type &value) {
     if (m_size == m_capacity) {
       size_t newCapacity = (m_capacity == 0) ? 1 : m_capacity * 2;
-      T *newData = new T[newCapacity];
+      value_type *newData = new value_type[newCapacity];
       for (size_t i = 0; i < m_size; ++i) {
         newData[i] = m_data[i];
       }
@@ -36,20 +44,26 @@ void XVector<T>::PushBack(const T &value) {
     m_data[m_size++] = value;
 }
 
-template <typename T>
-T &XVector<T>::operator[](size_t index) {
+template <typename Traits>
+typename Traits::value_type &XVector<Traits>::operator[](size_t index) {
     if (index >= m_size) {
       throw out_of_range("Index out of range");
     }
     return m_data[index];
 }
 
-template <typename T>
-ostream &operator<<(ostream &os, XVector<T> &v){
-    auto size = v.Size();
+template <typename Traits>
+string XVector<Traits>::toString() const {
+    string s = "";
+    auto size = Size();
     for (size_t i = 0; i < size; ++i)
-        os << v[i] << " ";
-    return os;
+        s += to_string(m_data[i]) + " ";
+    return s;
+}
+
+template <typename Traits>
+ostream &operator<<(ostream &os, XVector<Traits> &v){
+    return os << v.toString();
 }
 
 void DemoVector();
