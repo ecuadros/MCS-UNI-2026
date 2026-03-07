59bfa47d01fa074d74c0ea0ca3c42dd69ef92eb8
ecuadros <ecuadros@spc.org.pe>
2026-03-06 20:50:21 -0500

    feat: Implement variadic template ForEach and add thread-safe resizing with mutexes to XVector.

--- containers/xvector.cpp
+++ containers/xvector.cpp
@@ -1,44 +1,62 @@
-#include <fstream>
 #include "xvector.h"
 #include "../types.h"
+#include <fstream>
 
-template<typename T>
-void Add(T& val) { val += 5;  }
+template <typename T> void Add(T &val) { val += 5; }
 
-template<>
-void Add(T3& val) { val += "-XYZ";  }
+template <> void Add(T3 &val) { val += "-XYZ"; }
+
+template <typename... Args>
+auto Sum(Args... args){
+    return (args + ...);
+}
+
+template <typename T, typename... Args>
+void AddX(T &val, Args... args){
+    val += Sum(args...);
+}
 
 void DemoVector() {
-    XVector< VectorTraits<T1> > v1;
-    auto x = 20;
-    v1.PushBack(5);
-    v1.PushBack(2);
-    v1.PushBack(8);
-    v1[1] = 15;
-    cout << v1 << endl;
-    v1.PushBack(10); 
-    v1.ForEach(&Add);
-    cout << v1 << "Otro texto: " << x << endl;
+  XVector<VectorTraits<T1>> v1;
+  auto x = 20;
+  v1.PushBack(5);
+  v1.PushBack(2);
+  v1.PushBack(8);
+  v1[1] = 15;
+  cout << v1 << endl;
+  v1.PushBack(10);
+  //   v1.ForEach(&::AddX, 10, 30, 50, 20, 10, 5);
+  cout << v1 << endl;
+//   v1.ForEach(&::AddX, -10, -300);
 
-    XVector< VectorTraits<T2> > v2;
-    v2.PushBack(3.5);
-    v2.PushBack(2.5);
-    v2.PushBack(1.5);
-    v2.ForEach(&Add);
-    cout << v2 << endl;
+  v1.ForEachOld(&Add);
+  cout << v1 << "Otro texto: " << x << endl;
 
-    XVector< VectorTraits<T3> > v3;
-    v3.PushBack("Hola");
-    v3.PushBack("MCS"); 
-    v3.PushBack("UNI");
-    v3.PushBack("2026");
-    v3.ForEach(&Add);
-    cout << v3 << endl;
+  XVector<VectorTraits<T2> > v2;
+  v2.PushBack(3.5);
+  v2.PushBack(2.5);
+  v2.PushBack(1.5);
+  v2.ForEachOld(&Add);
+  cout << v2 << endl;
 
-    ofstream of("Output.txt");
-    of << v1 << endl;
-    of << v2 << endl;
-    of << v3 << endl;
-    of.close();
+  v2.ForEachOld([](T2 &val) { val += 10; });
+  cout << v2 << endl;
+  // TODO:
+  // T2 extra = 20;
+  // v2.ForEachOld([extra](T2& val){ val += extra; });
+  // cout << v2 << endl;
 
+  XVector<VectorTraits<T3>> v3;
+  v3.PushBack("Hola");
+  v3.PushBack("MCS");
+  v3.PushBack("UNI");
+  v3.PushBack("2026");
+  v3.ForEachOld(&Add);
+  cout << v3 << endl;
+
+  ofstream of("Output.txt");
+  of << v1 << endl;
+  of << v2 << endl;
+  of << v3 << endl;
+  of.close();
 }
--- containers/xvector.h
+++ containers/xvector.h
@@ -2,94 +2,103 @@
 #define __XVECTOR_H__
 
 #include <iostream>
-#include <string>
 #include <sstream>
+#include <string>
+#include <mutex>
 
 using namespace std;
 
-template <typename T>
-struct VectorTraits {
-    using value_type = T;
-};
+template <typename T> struct VectorTraits { using value_type = T; };
 
-template <typename Traits>
-class XVector {
-    using value_type = typename Traits::value_type;
+template <typename Traits> class XVector {
+  using value_type = typename Traits::value_type;
 
 private:
-    value_type     *m_data;
-    size_t          m_size;
-    size_t          m_capacity;
+  value_type *m_data;
+  size_t m_size;
+  size_t m_capacity;
+
+  mutex mtx;
 
 public:
-    XVector() : m_data(nullptr), m_size(0), m_capacity(0) {}
-    virtual ~XVector() { delete[] m_data; }
+  XVector() : m_data(nullptr), m_size(0), m_capacity(0) {}
+  virtual ~XVector() { delete[] m_data; }
 
-    void PushBack(const value_type &value);
-    value_type &operator[](size_t index);
-    size_t Size() const { return m_size; }
-    string toString() const;
-    string GetClassName() const { return string("XVector"); };
+  void PushBack(const value_type &value);
+  void Resize();
+  value_type &operator[](size_t index);
+  size_t Size() const { return m_size; }
+  string toString() const;
+  string GetClassName() const { return string("XVector"); };
 
-    void ForEach(void (*func)(value_type &));
+  template <typename Func, typename... Args>
+  void ForEach(Func func, Args... args){
+    for (size_t i = 0; i < m_size; ++i)
+        func(m_data[i], args...);
+  }
+  void ForEachOld(void (*func)(value_type &));
 };
 
 template <typename Traits>
-void XVector<Traits>::ForEach(void (*func)(value_type &)) {
+void XVector<Traits>::ForEachOld(void (*func)(value_type &)) {
+  for (size_t i = 0; i < m_size; ++i)
+    func(m_data[i]);
+}
+
+template <typename Traits>
+void XVector<Traits>::Resize() {
+    scoped_lock lock(mtx);
+    size_t newCapacity = (m_capacity == 0) ? 1 : m_capacity * 2;
+    value_type *newData = new value_type[newCapacity];
     for (size_t i = 0; i < m_size; ++i)
-        func(m_data[i]);
+        newData[i] = m_data[i];
+    delete[] m_data;
+    m_data = newData;
+    m_capacity = newCapacity;
 }
 
 template <typename Traits>
 void XVector<Traits>::PushBack(const value_type &value) {
-    if (m_size == m_capacity) {
-      size_t newCapacity = (m_capacity == 0) ? 1 : m_capacity * 2;
-      value_type *newData = new value_type[newCapacity];
-      for (size_t i = 0; i < m_size; ++i) {
-        newData[i] = m_data[i];
-      }
-      delete[] m_data;
-      m_data = newData;
-      m_capacity = newCapacity;
-    }
+    if (m_size == m_capacity)
+        Resize();
+    scoped_lock lock(mtx);
     m_data[m_size++] = value;
 }
 
 template <typename Traits>
 typename Traits::value_type &XVector<Traits>::operator[](size_t index) {
-    if (index >= m_size) {
-      throw out_of_range("Index out of range");
-    }
-    return m_data[index];
+  if (index >= m_size) {
+    throw out_of_range("Index out of range");
+  }
+  return m_data[index];
+}
+
+template <typename Traits> string XVector<Traits>::toString() const {
+  stringstream ss;
+  auto size = Size();
+  ss << GetClassName() << ":[";
+  for (size_t i = 0; i < size - 1; ++i)
+    ss << m_data[i] << " ";
+  if (size > 0)
+    ss << m_data[size - 1];
+  ss << "]";
+  return ss.str();
 }
 
 template <typename Traits>
-string XVector<Traits>::toString() const {
-    stringstream ss;
-    auto size = Size();
-    ss << GetClassName() << ":[";
-    for (size_t i = 0; i < size - 1; ++i)
-        ss << m_data[i] << " ";
-    if (size > 0)
-        ss << m_data[size - 1];
-    ss << "]";
-    return ss.str();
-}
-
-template <typename Traits>
-ostream &operator<<(ostream &os, XVector<Traits> &v){
-    return os << v.toString();
+ostream &operator<<(ostream &os, XVector<Traits> &v) {
+  return os << v.toString();
 }
 
 // FIXME: No se implemento correctamente
 // Operador para leer un XVector desde un stream
 template <typename Traits>
-istream &operator>>(istream &is, XVector<Traits> &v){
-    // value_type value;
-    // while (is >> value) {
-    //     v.PushBack(value);
-    // }
-    return is;
+istream &operator>>(istream &is, XVector<Traits> &v) {
+  // value_type value;
+  // while (is >> value) {
+  //     v.PushBack(value);
+  // }
+  return is;
 }
 
 void DemoVector();
